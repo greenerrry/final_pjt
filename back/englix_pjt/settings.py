a9e4c4a2f07ad60d43f7e061e54381c1ebccfaac
yoonji <rladbswl8668@naver.com>
2024-12-02 22:14:06 +0900

    final

--- back/englix_pjt/settings.py
+++ back/englix_pjt/settings.py
@@ -171,14 +171,7 @@ CACHES = {
 }
 
 
-TMDB_API_KEY = os.getenv("TMDB_API_KEY", "")
 
-OPENSUBTITLES_API_KEY = os.getenv(
-    "OPENSUBTITLES_API_KEY", "")
-OPENAI_API_KEY = os.getenv(
-    "OPENAI_API_KEY", "")
-GEMINI_API_KEY = os.getenv(
-    "GEMINI_API_KEY", "")
 
 
 SPEECH_TO_TEXT_API_KEY = os.path.join(
